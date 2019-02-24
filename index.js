4b2e0b296c6e2e4c84e708d33bcf13aae660c74d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-24 13:58:36 +0000

    Deploy franklai/torrent-rss to github.com/franklai/torrent-rss.git:gh-pages

---
+++ index.js
@@ -0,0 +1,61 @@
+const got = require('got');
+const he = require('he');
+const RSS = require('rss');
+
+class Parser {
+  constructor(url) { this.url = url; }
+  async get_html() {
+    const resp = await got(this.url);
+    return resp.body;
+  }
+  parse_links(html) {
+    const pattern = /magnet:[^"]+/g;
+    return html.match(pattern).map(v => he.decode(v));
+  }
+  get_filename(value) {
+    const pattern = /&dn=(.+?)&/;
+    const matched = value.match(pattern);
+    if (matched) {
+      return decodeURIComponent(matched[1]);
+    }
+    return '';
+  }
+  convert_to_rss(links) {
+    const options = {
+      title: 'just a title',
+
+    };
+    const feed = new RSS(options)
+
+        for (const link of links) {
+      const name = this.get_filename(link);
+
+      feed.item({
+        title: name,
+        url: link,
+        guid: link,
+      })
+    }
+
+    return feed.xml({indent: true});
+  }
+  async parse() {
+    const html = await this.get_html();
+
+    const magnet_links = this.parse_links(html);
+    const rss = this.convert_to_rss(magnet_links);
+
+    console.log(rss);
+  }
+}
+
+function main() {
+  const url = 'http://www.zimuxia.cn/portfolio/%E9%9F%A6%E9%A9%AE%E5%A4%A9';
+  parser = new Parser(url);
+
+  parser.parse().then(() => { });
+}
+
+if (require.main === module) {
+  main();
+}
