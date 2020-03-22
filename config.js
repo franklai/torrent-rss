4c4b3e6f9cd0ae1c9c0ac857ac4507605a7a8692
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2020-03-22 14:44:45 +0800

    add other bilibili link

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.links = [
-  ['Aloha字幕组', 'https://space.bilibili.com/14016058/video'],
+  ['Aloha字幕组 - 嵐にしやがれ', 'https://space.bilibili.com/14016058/video'],
+  ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['テセウスの船', 'http://www.zimuxia.cn/portfolio/忒修斯之船'],
   // ['Doctor-X', 'http://www.zimuxia.cn/portfolio/doctor-x'],
   // ['俺の話は長い', 'http://www.zimuxia.cn/portfolio/%E6%88%91%E7%9A%84%E4%BA%8B%E8%AF%B4%E6%9D%A5%E8%AF%9D%E9%95%BF'],
