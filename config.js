0fe32c27f1fb1dadf84acf4d3909e86b2d394477
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2020-04-12 20:12:21 +0800

    add VS arashi from 我想加个字幕组

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.links = [
   ['Aloha字幕组 - 嵐にしやがれ', 'https://space.bilibili.com/14016058/video'],
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
+  ['我想加个字幕组 - VS Arashi', 'https://space.bilibili.com/129426196'],
   ['テセウスの船', 'http://www.zimuxia.cn/portfolio/忒修斯之船'],
   // ['Doctor-X', 'http://www.zimuxia.cn/portfolio/doctor-x'],
   // ['俺の話は長い', 'http://www.zimuxia.cn/portfolio/%E6%88%91%E7%9A%84%E4%BA%8B%E8%AF%B4%E6%9D%A5%E8%AF%9D%E9%95%BF'],
