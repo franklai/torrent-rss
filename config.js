27b07cd74dbd3fdcaf5e583db9f6f41e4f27c2ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-09 10:06:37 +0000

    Deploy franklai/torrent-rss to github.com/franklai/torrent-rss.git:gh-pages

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ exports.links = [
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
   ['我想加个字幕组 - VS Arashi', 'https://space.bilibili.com/129426196'],
-  ['半澤直樹', 'http://www.zimuxia.cn/portfolio/半泽直树'],
-  ['派遣女王', 'http://www.zimuxia.cn/portfolio/派遣员的品格'],
+  ['LexBurner', 'https://space.bilibili.com/777536/video'],
+  // ['半澤直樹', 'http://www.zimuxia.cn/portfolio/半泽直树'],
+  // ['派遣女王', 'http://www.zimuxia.cn/portfolio/派遣员的品格'],
   // ['Doctor-X', 'http://www.zimuxia.cn/portfolio/doctor-x'],
   // ['俺の話は長い', 'http://www.zimuxia.cn/portfolio/%E6%88%91%E7%9A%84%E4%BA%8B%E8%AF%B4%E6%9D%A5%E8%AF%9D%E9%95%BF'],
 ];
