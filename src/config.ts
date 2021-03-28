27d60f74a6ed4637e47dc0c85207cac211172c10
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-03-29 07:42:47 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,7 @@
 export const links: [string, string][] = [
   ['Aloha', 'https://space.bilibili.com/14016058/video'],
-  ['小和财露尖尖嗓', 'https://space.bilibili.com/22191019/video'],
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
-  ['哇哇哇妹', 'https://space.bilibili.com/138832847/video'],
-  [
-    '書けないッ！？',
-    'http://www.zimuxia.cn/portfolio/%e5%86%99%e4%b8%8d%e5%87%ba%e6%9d%a5%e7%bc%96%e5%89%a7%e5%90%89%e4%b8%b8%e5%9c%ad%e4%bd%91%e6%b2%a1%e6%9c%89%e6%9d%a1%e7%90%86%e7%9a%84%e7%94%9f%e6%b4%bb',
-  ],
+  ['俺の家の話', 'http://www.zimuxia.cn/portfolio/%e6%88%91%e5%ae%b6%e7%9a%84%e6%95%85%e4%ba%8b'],
+  ['天国と地獄〜サイコな2人〜', 'http://www.zimuxia.cn/portfolio/%e5%a4%a9%e5%9b%bd%e4%b8%8e%e5%9c%b0%e7%8b%b1'],
 ];
