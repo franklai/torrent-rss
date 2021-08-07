79d0a1ee456f6c9a3b24ad32e78af01231222725
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-08-07 22:24:04 +0800

    update config
    - fix-sub: TOKYO MER
    - fix-sub: kintori

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,7 @@ export const links: [string, string][] = [
   ['Aloha', 'https://space.bilibili.com/14016058/video'],
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
-  ['ドラゴン桜', 'http://www.zimuxia.cn/portfolio/%e9%be%99%e6%a8%b1'],
+//  ['ドラゴン桜', 'http://www.zimuxia.cn/portfolio/%e9%be%99%e6%a8%b1'],
+  ['紧急审讯室', 'http://www.zimuxia.cn/portfolio/%e7%b4%a7%e6%80%a5%e5%ae%a1%e8%ae%af%e5%ae%a4'],
+  ['TOKYO MER', 'http://www.zimuxia.cn/portfolio/tokyo-mer-%e7%a7%bb%e5%8a%a8%e6%80%a5%e6%95%91%e5%ae%a4'],
 ];
