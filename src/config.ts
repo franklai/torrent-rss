f47cff28bcb616b20f620544452487d27541b232
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-09-23 12:13:18 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,6 @@ export const links: [string, string][] = [
 //  ['ドラゴン桜', 'http://www.zimuxia.cn/portfolio/%e9%be%99%e6%a8%b1'],
   ['紧急审讯室', 'http://www.zimuxia.cn/portfolio/%e7%b4%a7%e6%80%a5%e5%ae%a1%e8%ae%af%e5%ae%a4'],
   ['TOKYO MER', 'http://www.zimuxia.cn/portfolio/tokyo-mer-%e7%a7%bb%e5%8a%a8%e6%80%a5%e6%95%91%e5%ae%a4'],
+  ['ハコヅメ〜たたかう！交番女子〜', 'http://www.zimuxia.cn/portfolio/%e7%a7%98%e5%af%86%e5%86%85%e5%b9%95%e6%88%98%e6%96%97%e5%90%a7%e5%a5%b3%e5%b7%a1%e8%ad%a6'],
+  ['大豆田とわ子と三人の元夫', 'http://www.zimuxia.cn/portfolio/%e5%a4%a7%e8%b1%86%e7%94%b0%e5%8d%81%e5%92%8c%e5%ad%90%e4%b8%8e%e4%b8%89%e4%b8%aa%e5%89%8d%e5%a4%ab'],
 ];
