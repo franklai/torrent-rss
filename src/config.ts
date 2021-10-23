98abd73aa02ca34bf33360336292d4a32f2ed9fc
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-10-23 17:08:13 +0800

    fix eslint

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export const links: [string, string][] = [
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
   ['Doctor-X', 'https://www.zimuxia.cn/portfolio/doctor-x'],
-  ['Super Rich', 'https://www.zimuxia.cn/portfolio/super-rich'],  
+  ['Super Rich', 'https://www.zimuxia.cn/portfolio/super-rich'],
 ];
