72fa40bed05f77fdc68e66bc1cc554d198d73ff5
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-10-23 16:56:43 +0800

    add config
    - fixsub: doctor-x S07
    - fixsub: Super Rich

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ export const links: [string, string][] = [
   ['Aloha', 'https://space.bilibili.com/14016058/video'],
   ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
-//  ['ドラゴン桜', 'http://www.zimuxia.cn/portfolio/%e9%be%99%e6%a8%b1'],
+  ['Doctor-X', 'https://www.zimuxia.cn/portfolio/doctor-x'],
+  ['Super Rich', 'https://www.zimuxia.cn/portfolio/super-rich'],  
 ];
