1cbd2a1c56b31726acc098845da3e4e0addb8a88
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-11-08 08:13:28 +0800

    adjust config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const links: [string, string][] = [
   ['Aloha', 'https://space.bilibili.com/14016058/video'],
-  ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
-  ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
+//  ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
+//  ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
   ['Doctor-X', 'https://www.zimuxia.cn/portfolio/doctor-x'],
   ['Super Rich', 'https://www.zimuxia.cn/portfolio/super-rich'],
 ];
