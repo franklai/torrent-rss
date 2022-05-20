111227dcd92224a9681652d29f96d22aecb4e4b7
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2022-05-20 11:56:19 +0800

    adjust config
    - fixsub: 俺の可愛いはもうすぐ消費期限!?
    - fixsub: 未来への１０カウント

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const links: [string, string][] = [
   ['Aloha', 'https://space.bilibili.com/14016058/video'],
 //  ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
 //  ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
-  ['Doctor-X', 'https://www.zimuxia.cn/portfolio/doctor-x'],
-  ['Super Rich', 'https://www.zimuxia.cn/portfolio/super-rich'],
+  ['俺の可愛いはもうすぐ消費期限!?', 'https://www.zimuxia.cn/portfolio/%e6%88%91%e7%9a%84%e5%8f%af%e7%88%b1%e8%a6%81%e8%bf%87%e6%9c%9f%e4%ba%86],
+  ['未来への１０カウント', 'https://www.zimuxia.cn/portfolio/%e8%bf%88%e5%90%91%e6%9c%aa%e6%9d%a5%e7%9a%84%e5%80%92%e6%95%b010%e7%a7%92],
 ];
