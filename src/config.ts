453b7356fe36a97308568475c666e98c5e149fa6
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2025-01-07 08:07:44 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,14 @@
 export const links: [string, string][] = [
-  // ['阿泥妹饭', 'https://space.bilibili.com/4746559/video'],
+  ['阿泥妹饭', 'https://space.bilibili.com/4746559/video'],
+  ['饭圈八瓜娱', 'https://space.bilibili.com/3493298094213582/video'],
   //  ['NAN字幕組 - 夜會', 'https://space.bilibili.com/14147173/video'],
   //  ['有机星期一 - 月曜', 'https://space.bilibili.com/28259096/video'],
-  [
-    'vivant',
-    'https://www.zimuxia.cn/portfolio/vivant',
-  ],
-  [
-    'ブルーモーメント',
-    'https://www.zimuxia.cn/portfolio/%e8%93%9d%e8%89%b2%e6%97%b6%e5%88%bb',
-  ],
+  // [
+  //   'vivant',
+  //   'https://www.zimuxia.cn/portfolio/vivant',
+  // ],
+  // [
+  //   'ブルーモーメント',
+  //   'https://www.zimuxia.cn/portfolio/%e8%93%9d%e8%89%b2%e6%97%b6%e5%88%bb',
+  // ],
 ];
